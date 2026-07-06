1b30afb097620440e3c06080d9fc06b1cab0f95b
ronak gaikar <ronakgaikar4@gmail.com>
2026-07-06 17:28:23 +0530

    Add inventory item

--- inventory.py
+++ inventory.py
@@ -5,7 +5,12 @@ items_database = {
     "1001": {"name":"Bread", "price": 20},
     "1002": {"name":"milk",  "price": 30},
     "1003": {"name":"banana", "price":60},
-    "1004": {"name":"pen",    "price":10}
+    "1004": {"name":"pen",    "price":10},
+    "1005": {"name":"paneer", "price":240},
+    "1006": {"name":"aloo bhujiya", "price":56},
+    "1005": {"name":"curd", "price": 30},
+    "1005": {"name":"drinking water", "price":20},
+    "1005": {"name":"Thumbs-up", "price":40}
     }
 
 def get_item(barcode):
